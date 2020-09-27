3e5127d11792ac0cde9160806b1a9d01640f31f3
chris <nnchrich@163.com>
2020-09-27 12:28:28 +0800

    add firstcoreweb project

---
+++ projects/firstcoreweb/Controllers/ContentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using firstcoreweb.Models;
+
+namespace firstcoreweb.Controllers
+{
+    public class ContentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var contents = new List<Content>();
+            for (int i = 1; i < 11; i++)
+            {
+                contents.Add(new Content { Id=i,title=$"{i}的标题",content= $"{i}的内容",status=1, add_time=DateTime.Now.AddDays(-i)});
+            }
+            return View(new ContentViewModel { Contents=contents});
+        }
+    }
+}
---
+++ projects/firstcoreweb/Models/ContentViewModel.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace firstcoreweb.Models
+{
+   public class ContentViewModel
+    {
+        /// <summary>
+        /// 内容列表
+        /// </summary>
+        public List<Content> Contents { get; set; }
+    }
+  
+}
